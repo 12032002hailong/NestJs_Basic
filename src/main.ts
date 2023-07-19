268acd8908b506fe6e6440b073b2e6f9394feeb0
Hai Long <77374492+12032002hailong@users.noreply.github.com>
2023-07-19 23:08:42 +0700

    create company

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  //config CORS
+  app.enableCors(
+    {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
+    }
+  );
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
