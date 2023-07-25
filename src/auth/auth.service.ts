dd3365f203667c837292721ae7ca448934525be7
Hai Long <77374492+12032002hailong@users.noreply.github.com>
2023-07-25 16:00:48 +0700

    Update User Type (JWT)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,33 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import {JwtService} from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
+import { IUser } from 'src/users/user.interface';
 
 @Injectable()
 export class AuthService {
-    constructor(
-      private usersService: UsersService,
-      private jwtService: JwtService
-      ) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) { }
 
   //user /pass là 2 tham số thư viện pasport trả về
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOneByUsername(username);
-    if(user){
+    if (user) {
       const isValid = this.usersService.isValidPassword(pass, user.password)
-      if(isValid === true){
+      if (isValid === true) {
         return user;
       }
     }
     return null;
   }
 
-  async login(user: any){
+  async login(user: IUser) {
+    const { _id, name, email, role } = user;
     const payload = {
-      username: user.email,
-      sub: user._id
+      sub: "token login",
+      iss: "from server",
+      _id,
+      name,
+      email,
+      role
     };
-    return{
-      access_token: this.jwtService.sign(payload)
-    } 
+    return {
+      access_token: this.jwtService.sign(payload),
+      _id,
+      name,
+      email,
+      role
+    }
   }
 }
