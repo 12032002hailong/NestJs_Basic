4b0e20ed9f3d31c9e44b7bb61f2dcc9f142576f9
Hai Long <77374492+12032002hailong@users.noreply.github.com>
2023-08-01 22:07:43 +0700

    Version Api

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptors';
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.useStaticAssets(join(__dirname, '..', 'public')); //js, css, image
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // view
@@ -30,6 +31,15 @@ async function bootstrap() {
       "optionsSuccessStatus": 204
     }
   );
+
+
+  //config versioning
+  app.setGlobalPrefix('api')
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2']
+  })
+
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
